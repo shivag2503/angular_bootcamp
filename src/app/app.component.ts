77a00e96442501f8d4a557d865dcfc5a0e8252b3
shivam2503 <shivam.agrawal@technonjr.org>
2021-05-07 20:42:32 +0530

    new feature added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  
+  posts = [
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpeg",
+      username: "nature",
+      content: "Today I saw a beautiful green tree."
+    },
+    {
+      title: "Snowy Mountain",
+      imageUrl: "assets/mountain.jpeg",
+      username: "mountainlover",
+      content: "Here is a picture of snowy mountain."
+    },
+    {
+      title: "Mountain Biking",
+      imageUrl: "assets/biking.jpeg",
+      username: "biking12255",
+      content: "Today I done some biking."
+    }
+  ];
 }
