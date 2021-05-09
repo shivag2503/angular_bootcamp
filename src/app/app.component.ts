0691b263816cbd0f73393b8208bf785346ecc516
shivam2503 <shivam.agrawal@technonjr.org>
2021-05-09 17:48:42 +0530

    pipes implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name = '';
+  date = '';
+  amount = 0;
+
+  onChangeName(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: number) {
+    this.amount = value;
+  }
+
 }
